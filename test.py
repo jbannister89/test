f19035c8442059a6a7c2f67e77852acada859d6b
jbannister89 <jbannister@orbitage.com>
2022-12-01 11:34:33 +0800

    Add files via upload

---
+++ test.py
@@ -0,0 +1,14 @@
+x = 1
+p = 4
+q = 5
+if x==p:
+    if p <= q:
+        if q > 1:
+            if p >1:
+                x = q
+    else:
+        x = p
+if x >= q:
+    print(x)
+else:
+    print(q)
